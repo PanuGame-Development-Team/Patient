2fbf60ec2eb462cf62e7e834bd298334c1531ed3
Zara Zorin <BillsgWu@outlook.com>
2025-07-29 20:16:32 +0800

    [fix]

--- Patient/urls.py
+++ Patient/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.shortcuts import redirect
 from user.urls import urlpatterns as userurl
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", lambda request:redirect("/record/",permanent=True)),
     path("record/",include("record.urls")),
     *userurl
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- autoinst.sh
+++ autoinst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p static/exports
 mkdir -p static/uploads
-pip install -r requirements.txt
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 touch Patient/properties.py
 python3 manage.py makemigrations
 python3 manage.py migrate
